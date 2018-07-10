c859bef6713928b65306bb534af084123a29e075
mrstephenneal <stephen.neal14@gmail.com>
2018-07-10 10:17:34 -0400

    Added directory parameter to dict.py so that working directory does not need to change prior to call.

--- databasetools/dict/dict.py
+++ databasetools/dict/dict.py
@@ -3,13 +3,14 @@ import os
 
 
 class DictTools:
-    def __init__(self, name):
+    def __init__(self, directory, name):
         """
         Save or load data to or from .npy dictionary file
+        :param directory: Directory to save or load file from
         :param name: File name
         """
         self.dict = dict
-        self.save_name = name + ".npy"
+        self.save_name = os.path.join(directory, str(name + ".npy"))
 
     def __iter__(self):
         return iter(self.read_dictionary)
@@ -17,7 +18,7 @@ class DictTools:
     def save(self, dict):
         try:
             np.save(self.save_name, dict)
-            print("\nnpy saved to " + os.getcwd())
+            print("\nnpy saved to " + self.save_name)
         except:
             print("\nnpy not saved")
 
--- setup.py
+++ setup.py
@@ -2,15 +2,16 @@ from setuptools import setup, find_packages
 
 setup(
     name='databasetools',
-    version='0.3.9',
+    version='0.3.10',
     packages=find_packages(),
     install_requires=[
-              'looptools',
-              'mysql-connector==2.1.6',
-              'numpy',
-              'pandas',
-              'tqdm',
-          ],
+        'looptools',
+        'mysql-connector==2.1.6',
+        'numpy',
+        'pandas',
+        'tqdm',
+        'pathlib',
+    ],
     url='https://github.com/mrstephenneal/databasetools.git',
     license='MIT License',
     author='Stephen Neal',
