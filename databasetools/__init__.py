5f643dbc5e95888da4adf0fd96d9872fb9e98ed3
mrstephenneal <stephen.neal14@gmail.com>
2018-08-16 16:08:11 -0400

    Added JSON to package __init__.py

--- databasetools/__init__.py
+++ databasetools/__init__.py
@@ -3,7 +3,8 @@ from databasetools.sqlite import SQLiteSyntax, SQLiteQuery, SQLiteTools
 from databasetools.dict import DictTools
 from databasetools.mysql import MySQLTools
 from databasetools.php import PHPArray
+from databasetools.json import JSON
 
 __name__ = "Database Tools"
 __all__ = ["CSVExport", "CSVImport", "CSV", "SQLiteSyntax", "SQLiteQuery", "SQLiteTools", "DictTools", "MySQLTools",
-           "PHPArray"]
+           "PHPArray", "JSON"]
