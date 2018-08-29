e044e1ea4370e12bc073bd6e7b01d9a7b8209ac8
mrstephenneal <stephen.neal14@gmail.com>
2018-08-29 15:27:32 -0400

    Fixed mysql.py import error and renamed to sql.py

--- databasetools/__init__.py
+++ databasetools/__init__.py
@@ -1,7 +1,7 @@
 from databasetools.csv import CSVExport, CSVImport, CSV
 from databasetools.sqlite import SQLiteSyntax, SQLiteQuery, SQLiteTools
 from databasetools.dict import DictTools
-from databasetools.mysql import MySQLTools
+from databasetools.sql import MySQLTools
 from databasetools.php import PHPArray
 from databasetools.json import JSON
 from databasetools.pickle import Pickle
