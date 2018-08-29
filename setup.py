9dc6a0656ebf856a2af69286687df421a0589681
mrstephenneal <stephen.neal14@gmail.com>
2018-08-29 14:44:31 -0400

    Removed numpy as a dependency and updated description

--- setup.py
+++ setup.py
@@ -6,16 +6,14 @@ setup(
     packages=find_packages(),
     install_requires=[
         'looptools',
-        'mysql-connector==2.1.6',
-        'numpy',
+        'mysql-connector>=2.1.6',
         'tqdm',
     ],
     url='https://github.com/mrstephenneal/databasetools',
     license='MIT License',
     author='Stephen Neal',
     author_email='stephen@stephenneal.net',
-    description='Additional functionality added to DictTools allowing for dictionaries to be saved in numpy, '
-                'json or pickle format.',
+    description='Convenient data storage toolkit.',
     long_description='A collection of database tools written in Python for handling basic actions with CSV files, '
                 'numpy dictionaries, SQLite databases and MySQL databases.'
 )
