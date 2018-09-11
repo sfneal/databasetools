980a268fcda807daea728542c2fd98e40504ba01
mrstephenneal <stephen.neal14@gmail.com>
2018-09-11 15:30:42 -0400

    Updated to version 0.4.8

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='databasetools',
-    version='0.4.7',
+    version='0.4.8',
     packages=find_packages(),
     install_requires=[
         'looptools',
