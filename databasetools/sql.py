e37ee400356c2e38b378f9c6451a4fc74d811bb0
mrstephenneal <stephen.neal14@gmail.com>
2018-09-25 09:52:38 -0400

    Added docstring comments to methods

--- databasetools/sql.py
+++ databasetools/sql.py
@@ -47,16 +47,16 @@ class MySQLTools:
         self._close()
 
     def _connect(self, config):
+        """Establish a connection with a MySQL database."""
         try:
             self._cnx = mysql.connector.connect(**config)
             self._cursor = self._cnx.cursor()
             self._printer('\tMySQL DB connection established')
-
         except mysql.connector.Error as err:
             if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-                self._printer("Something is wrong with your user name or password")
+                print("Something is wrong with your user name or password")
             elif err.errno == errorcode.ER_BAD_DB_ERROR:
-                self._printer("Database does not exist")
+                print("Database does not exist")
             raise err
 
     def _printer(self, msg):
@@ -65,25 +65,30 @@ class MySQLTools:
             print(msg)
 
     def _close(self):
+        """Close MySQL database connection."""
         self._cursor.close()
         self._cnx.close()
 
     def _commit(self):
+        """Commit the changes made during the current connection."""
         self._cnx.commit()
 
     def _fetch(self, statement):
+        """Execute a SQL query and return values."""
         # Execute statement
         self._cursor.execute(statement)
         self._printer('\tMySQL rows successfully queried')
         return [row for row in self._cursor]
 
     def select(self, table, cols):
+        """Query only certain columns from a table and every row."""
         # Concatenate statement
         cols_str = join_columns(cols)
         statement = ("SELECT " + cols_str + " FROM " + str(table))
         return self._fetch(statement)
 
     def select_where(self, table, cols, where):
+        """Query certain columns from a table where a particular value is found."""
         # Either join list of columns into string or set columns to * (all)
         if isinstance(cols, list):
             cols_str = join_columns(cols)
@@ -97,18 +102,20 @@ class MySQLTools:
         self._fetch(statement)
 
     def select_all(self, table):
+        """Query all rows and columns from a table."""
         # Concatenate statement
         statement = ("SELECT * FROM " + str(table))
         return self._fetch(statement)
 
     def select_all_join(self, table1, table2, key):
+        """Left join all rows and columns from two tables where a common value is shared."""
         # TODO: Write function to run a select * left join query
         pass
 
     def insert(self, table, columns, values):
-        cols, vals = get_column_value_strings(columns)
-
+        """Insert a singular row into a table"""
         # Concatenate statement
+        cols, vals = get_column_value_strings(columns)
         statement = ("INSERT INTO " + str(table) + "(" + cols + ") " + "VALUES (" + vals + ")")
 
         # Execute statement
@@ -116,9 +123,13 @@ class MySQLTools:
         self._printer('\tMySQL row successfully inserted')
 
     def insert_many(self, table, columns, values):
-        cols, vals = get_column_value_strings(columns)
+        """
+        Insert multiple rows into a table.
 
+        If only one row is found, self.insert method will be used.
+        """
         # Concatenate statement
+        cols, vals = get_column_value_strings(columns)
         statement = ("INSERT INTO " + str(table) + "(" + cols + ") " + "VALUES (" + vals + ")")
 
         # Execute statement
@@ -126,6 +137,7 @@ class MySQLTools:
         self._printer('\tMySQL rows (' + str(len(values)) + ') successfully INSERTED')
 
     def update(self, table, columns, values, where):
+        """Update the values of a particular row where a value is met."""
         where_col, where_val = where  # Unpack WHERE clause dictionary into tuple
         cols = get_column_value_strings(columns, query_type='update')  # Get SET clause string
 
@@ -137,6 +149,7 @@ class MySQLTools:
         self._printer('\tMySQL rows (' + str(len(values)) + ') successfully UPDATED')
 
     def truncate(self, table):
+        """Empty a table by deleting all of its rows."""
         statement = "TRUNCATE " + str(table)
         self._cursor.execute(statement)
         self._printer('\tMySQL table ' + str(table) + ' successfully truncated')
