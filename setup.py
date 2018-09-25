0a6a5ca3a1658117e7d901611899cddf5698f977
mrstephenneal <stephen.neal14@gmail.com>
2018-09-25 10:39:14 -0400

    Updated to version 1.0.0
    All basic functionality has been built out

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='databasetools',
-    version='0.4.12',
+    version='1.0.0',
     packages=find_packages(),
     install_requires=[
         'mysql-connector>=2.1.6',
