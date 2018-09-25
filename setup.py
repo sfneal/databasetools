a529c43a345ee88d0d05215ade89cff651e4c172
mrstephenneal <stephen.neal14@gmail.com>
2018-09-25 10:29:50 -0400

    Updated to PyPi version 0.4.12
    Fixed insert_many method of MySQLTools

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='databasetools',
-    version='0.4.11',
+    version='0.4.12',
     packages=find_packages(),
     install_requires=[
         'mysql-connector>=2.1.6',
