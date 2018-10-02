4b8d99eee754e10ddc3f4268ca69c7d51c73bc69
mrstephenneal <stephen.neal14@gmail.com>
2018-10-02 11:26:11 -0400

    Updated to PyPi version 1.1.0
    Added execute_sql_script method to MySQLTools to run a sql file.
    Added update method to JSON to update a particular key's value.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='databasetools',
-    version='1.0.0',
+    version='1.1.0',
     packages=find_packages(),
     install_requires=[
         'mysql-connector>=2.1.6',
