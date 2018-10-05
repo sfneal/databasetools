00e40877e2972abed3eca6e788e39ff404802a98
mrstephenneal <stephen.neal14@gmail.com>
2018-10-05 14:18:38 -0400

    Refactored sql module to mysql

--- databasetools/__init__.py
+++ databasetools/__init__.py
@@ -1,7 +1,7 @@
 from databasetools.csv import CSVExport, CSVImport, CSV
 from databasetools.sqlite import SQLiteSyntax, SQLiteQuery, SQLiteTools
 from databasetools.dict import DictTools
-from databasetools.sql import MySQL, MySQLTools
+from databasetools.mysql import MySQL, MySQLTools
 from databasetools.php import PHPArray
 from databasetools.json import JSON
 from databasetools.pickle import Pickle
