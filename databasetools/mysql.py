10b31f2eef0e11d35020329397d36af8703b9580
mrstephenneal <stephen.neal14@gmail.com>
2018-10-05 15:18:28 -0400

    Reordered method declarations

--- databasetools/mysql.py
+++ databasetools/mysql.py
@@ -64,41 +64,6 @@ class MySQL:
             print('\tMySQL disconnected')
 
     # ------------------------------------------------------------------------------
-    #                                GETTER METHODS                                |
-    # ------------------------------------------------------------------------------
-    @property
-    def tables(self):
-        """Retrieve a list of tables in the connected database"""
-        statement = 'show tables'
-        return self._fetch(statement)
-
-    @property
-    def databases(self):
-        """Retrieve a list of databases that are accessible under the current connection"""
-        return self._fetch('show databases')
-
-    def get_primary_key(self, table):
-        """Retrieve the column which is the primary key for a table."""
-        for column in self.get_schema(table):
-            if 'pri' in column[3].lower():
-                return column[0]
-
-    def get_primary_key_values(self, table):
-        """Retrieve a list of primary key values in a table"""
-        return self.select(table, self.get_primary_key(table), _print=False)
-
-    def count_rows(self, table):
-        """Get the number of rows in a particular table"""
-        return self.select(table, 'COUNT(*)', False)
-
-    def count_rows_all(self):
-        """Get the number of rows for every table in the database."""
-        return {table: self.count_rows(table) for table in self.tables}
-    # ------------------------------------------------------------------------------
-    #                                END GETTER METHODS                            |
-    # ------------------------------------------------------------------------------
-
-    # ------------------------------------------------------------------------------
     # |                            HELPER METHODS                                  |
     # ------------------------------------------------------------------------------
     def _connect(self, config):
@@ -156,6 +121,41 @@ class MySQL:
         self._commit()
     # ------------------------------------------------------------------------------
     # |                            END HELPER METHODS                              |
+    # ------------------------------------------------------------------------------
+
+    # ------------------------------------------------------------------------------
+    #                                GETTER METHODS                                |
+    # ------------------------------------------------------------------------------
+    @property
+    def tables(self):
+        """Retrieve a list of tables in the connected database"""
+        statement = 'show tables'
+        return self._fetch(statement)
+
+    @property
+    def databases(self):
+        """Retrieve a list of databases that are accessible under the current connection"""
+        return self._fetch('show databases')
+
+    def get_primary_key(self, table):
+        """Retrieve the column which is the primary key for a table."""
+        for column in self.get_schema(table):
+            if 'pri' in column[3].lower():
+                return column[0]
+
+    def get_primary_key_values(self, table):
+        """Retrieve a list of primary key values in a table"""
+        return self.select(table, self.get_primary_key(table), _print=False)
+
+    def count_rows(self, table):
+        """Get the number of rows in a particular table"""
+        return self.select(table, 'COUNT(*)', False)
+
+    def count_rows_all(self):
+        """Get the number of rows for every table in the database."""
+        return {table: self.count_rows(table) for table in self.tables}
+    # ------------------------------------------------------------------------------
+    #                                END GETTER METHODS                            |
     # ------------------------------------------------------------------------------
 
     # ------------------------------------------------------------------------------
