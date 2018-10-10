1057ef1ad9149e713edcc56dcd5dffb0914e99bf
mrstephenneal <stephen.neal14@gmail.com>
2018-10-10 13:04:51 -0400

    Changed comment

--- databasetools/mysql.py
+++ databasetools/mysql.py
@@ -1,7 +1,7 @@
 from mysql.toolkit import MySQL
 
 
-# TODO: Remove prior to 1.4 release
+# TODO: Remove prior to 1.5 release
 class MySQLTools(MySQL):
     def __init__(self, config, enable_printing=True):
         """Wrapper class for MySQL"""
